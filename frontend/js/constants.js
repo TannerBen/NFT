60a451b21527cccbe4c3205addc2d88d29397cb4
TannerBen <benedettotanner@gmail.com>
2022-05-26 20:48:45 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xc15E065708bE71eC0b0af29D8C4CC37385A88909"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
